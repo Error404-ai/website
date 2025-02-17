40c966c86121bf2130b3185ab4852f786eeb9489
rockyhappy <rachit22153019@akgec.ac.in>
2025-02-17 22:50:15 +0530

    Nav Edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item> */}
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="https://register.programming-club.tech/" 
                 // target="_blank"
@@ -102,7 +102,7 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Register
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             {/* <Nav.Item className="fork-btn">
               <Button
